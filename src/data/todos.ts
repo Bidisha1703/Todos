2347dc9bffb5c0f5fbd89b7629003c13c03368ab
Bidisha1703 <kundubidisha568@gmail.com>
2025-07-17 10:17:23 +0530

    first commit

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+import type { Todo } from "../types/todo";
+
+export const dummyData : Todo[] = [
+
+    {
+        id: 1,
+        title: "like this video ",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "subscribe to coding in flow",
+        completed: false,
+    },
+    {
+         id: 3,
+         title: "leave a nice comment",
+         completed: true,
+    },
+];
